3b415fc0882711d8647e128f999b25fd9846daf2
BenjaminE <benjamin.ehnes@fh-erfurt.de>
2022-09-29 10:05:46 +0200

    improved unit tests

--- Templates/WebAPI/Devon4Net.Test/Test/UnitTest/Business/DishManagement/Controllers/DishControllerTest.cs
+++ Templates/WebAPI/Devon4Net.Test/Test/UnitTest/Business/DishManagement/Controllers/DishControllerTest.cs
@@ -1,6 +1,7 @@
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Devon4Net.Test.xUnit.Test.Integration;
 using Xunit;
 using Devon4Net.Application.WebAPI.Implementation.Business.DishManagement.Service;
@@ -17,6 +18,7 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
 {
     public class DishControllerTest : UnitTest
     {
+        /*
         [Fact]
         public async void DishSearch_Returns_Result()
         {
@@ -59,12 +61,6 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
             input.Categories = new CategorySearchDto[1];
             input.Categories[0] = new CategorySearchDto();
             input.Categories[0].Id = 1;
-
-
-            /*
-            string expectedResult = @"{
-                'pagination':{'size':0,'page':0,'total':0},'content':[]}";
-            */
 
             var expectedResult = @"{""pagination"":{""size"":0,""page"":0,""total"":3},""content"":[{""dish"":{""id"":0,""modificationCounter"":0,""revision"":null,""name"":""burger"",""description"":null,""price"":6.0,""imageId"":null},""image"":{""id"":0,""modificationCounter"":null,""revision"":null,""name"":null,""content"":null,""contentType"":null,""mimeType"":null},""test"":null,""extras"":[],""categories"":[]},{""dish"":{""id"":0,""modificationCounter"":0,""revision"":null,""name"":""pizza"",""description"":null,""price"":8.0,""imageId"":null},""image"":{""id"":0,""modificationCounter"":null,""revision"":null,""name"":null,""content"":null,""contentType"":null,""mimeType"":null},""test"":null,""extras"":[],""categories"":[]},{""dish"":{""id"":0,""modificationCounter"":0,""revision"":null,""name"":""salad"",""description"":null,""price"":5.0,""imageId"":null},""image"":{""id"":0,""modificationCounter"":null,""revision"":null,""name"":null,""content"":null,""contentType"":null,""mimeType"":null},""test"":null,""extras"":[],""categories"":[]}]}";
 
@@ -117,6 +113,272 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
 
             Assert.Equal(expectedResult, result.Value);
         }
+        */
+        private readonly Random rand = new();
 
+        [Fact]
+        public async Task DishSearch_WithGivenCategoryFilterCriteria_ReturnsAllCorrespondingDishes()
+        {
+            //Arrange
+            var dishes = new[] { CreateRandomDish(), CreateRandomDish(), CreateRandomDish() };
+            var randomPrice = (decimal)rand.NextDouble();
+            var minLikes = rand.Next();
+            var searchBy = Guid.NewGuid().ToString();
+
+            List<long> categoryIds = new List<long>() { dishes[0].DishCategory.ToList().FirstOrDefault().Id, dishes[1].DishCategory.ToList().FirstOrDefault().Id };
+
+            var categorySearchDtoIds = categoryIds.Select(c => new CategorySearchDto
+            {
+                Id = c,
+            }).ToList().ToArray();
+
+            List<Dish> expectedDishes = dishes.Where(dish => dish.DishCategory.Any(cat => categoryIds.Contains(cat.Id))).ToList();
+
+            var serviceStub = new Mock<IDishService>();
+            serviceStub.Setup(repo => repo.GetDishesMatchingCriterias(
+                    //It.IsAny<decimal>(),
+                    It.IsAny<decimal>(),
+                    It.IsAny<int>(),
+                    It.IsAny<string>(),
+                    It.Is<IList<long>>(categoryIdList => categoryIdList.All(cat => categoryIds.Contains(cat)))
+                    )).Returns(Task.FromResult(expectedDishes));
+
+            var controller = new DishController(serviceStub.Object);
+            //Act
+            var filterDto = new FilterDtoSearchObjectDto { MaxPrice = randomPrice, SearchBy = searchBy, MinLikes = minLikes, Categories = categorySearchDtoIds };
+            var expectedResult = new ResultObjectDto<DishDtoResult> { };
+            expectedResult.content = expectedDishes.Select(DishConverter.EntityToApi).ToList();
+            expectedResult.Pagination.Total = expectedDishes.Count();
+
+            var result = (ObjectResult)await controller.DishSearch(filterDto);
+
+            //Assert
+            var compareObj = JsonConvert.SerializeObject(expectedResult);
+            Assert.Equal(compareObj, result.Value);
+        }
+
+        [Fact]
+        public async Task DishSearch_WithGivenMaxPriceFilterCriteria_ReturnsAllCorrespondingDishes()
+        {
+            //Arrange
+            var Dishes = new[] { CreateRandomDish(), CreateRandomDish(), CreateRandomDish() };
+            var DishPrices = new[] { Dishes[0].Price, Dishes[1].Price, Dishes[2].Price };
+
+            //Price Filter is set to max. Therefore all dishes will be taken into account.
+            var expectedPrice = DishPrices.Max();
+            var minLikes = rand.Next();
+            var searchBy = Guid.NewGuid().ToString();
+            var expectedCategories = Array.Empty<ICollection<Category>>();
+            IList<long> categoryObjIds = new List<long>() { };
+            var categorySearchDtoIds = categoryObjIds.Select(c => new CategorySearchDto
+            {
+                Id = c,
+            }).ToList().ToArray();
+
+            List<Dish> expectedDishes = Dishes.Where(dish => dish.Price <= expectedPrice).ToList();
+
+            var serviceStub = new Mock<IDishService>();
+            serviceStub.Setup(repo => repo.GetDishesMatchingCriterias(
+                    //It.IsAny<decimal>(),
+                    It.Is<decimal>(price => price == expectedPrice),
+                    It.IsAny<int>(),
+                    It.IsAny<string>(),
+                    It.IsAny<IList<long>>())).Returns(Task.FromResult(expectedDishes));
+
+            var controller = new DishController(serviceStub.Object);
+            //Act
+            var filterDto = new FilterDtoSearchObjectDto { MaxPrice = expectedPrice, SearchBy = searchBy, MinLikes = minLikes, Categories = categorySearchDtoIds };
+            var expectedResult = new ResultObjectDto<DishDtoResult> { };
+            expectedResult.content = expectedDishes.Select(DishConverter.EntityToApi).ToList();
+            expectedResult.Pagination.Total = expectedDishes.Count();
+
+            var result = (ObjectResult)await controller.DishSearch(filterDto);
+
+            //Assert
+            var compareObj = JsonConvert.SerializeObject(expectedResult);
+            Assert.Equal(compareObj, result.Value);
+        }
+
+        
+        [Fact]
+        public async Task DishSearch_WithGivenMinPriceFilterCriteria_ReturnsAllCorrespondingDishes()
+        {
+            //Arrange
+            var dishes = new[] { CreateRandomDish(), CreateRandomDish(), CreateRandomDish() };
+            var dishPrices = new[] { dishes[0].Price, dishes[1].Price, dishes[2].Price };
+
+            //Price Filter is set to min. Therefore only the cheapest dish(es) will be taken into account.
+            var expectedPrice = dishPrices.Min();
+            var minLikes = rand.Next();
+            var searchBy = Guid.NewGuid().ToString();
+            var expectedCategories = Array.Empty<ICollection<Category>>();
+            IList<long> categoryObjIds = new List<long>() { };
+            var categorySearchDtoIds = categoryObjIds.Select(c => new CategorySearchDto
+            {
+                Id = c,
+            }).ToList().ToArray();
+
+            List<Dish> expectedDishes = dishes.Where(dish => dish.Price <= expectedPrice).ToList();
+
+            var serviceStub = new Mock<IDishService>();
+            serviceStub.Setup(repo => repo.GetDishesMatchingCriterias(
+                    //It.IsAny<decimal>(),
+                    It.Is<decimal>(price => price == expectedPrice),
+                    It.IsAny<int>(),
+                    It.IsAny<string>(),
+                    It.IsAny<IList<long>>())).Returns(Task.FromResult(expectedDishes));
+
+            var controller = new DishController(serviceStub.Object);
+            //Act
+            var filterDto = new FilterDtoSearchObjectDto { MaxPrice = expectedPrice, SearchBy = searchBy, MinLikes = minLikes, Categories = categorySearchDtoIds };
+            var expectedResult = new ResultObjectDto<DishDtoResult> { };
+            expectedResult.content = expectedDishes.Select(DishConverter.EntityToApi).ToList();
+            expectedResult.Pagination.Total = expectedDishes.Count();
+
+            var result = (ObjectResult)await controller.DishSearch(filterDto);
+
+            //Assert
+            var compareObj = JsonConvert.SerializeObject(expectedResult);
+            Assert.Equal(compareObj, result.Value);
+        }
+
+        [Fact]
+        public async Task DishSearch_WithGivenSearchByCriteria_ReturnsAllCorrespondingDishes()
+        {
+            //Arrange
+            var dishes = new[] { CreateRandomDish(), CreateRandomDish(), CreateRandomDish() };
+            var expectedPrice = (decimal)rand.NextDouble();
+            var minLikes = rand.Next();
+            //Choose randomly one of the Dish names to filter for
+            var dishNames = new[] { dishes[0].Name, dishes[1].Name, dishes[2].Name };
+            var searchBy = dishNames[rand.Next(0, 3)];
+
+            var expectedCategories = Array.Empty<ICollection<Category>>();
+            IList<long> categoryObjIds = new List<long>() { };
+            var categorySearchDtoIds = categoryObjIds.Select(c => new CategorySearchDto
+            {
+                Id = c,
+            }).ToList().ToArray();
+
+            List<Dish> expectedDishes = dishes.Where(dish => dish.Name.Contains(searchBy, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+            var serviceStub = new Mock<IDishService>();
+            serviceStub.Setup(repo => repo.GetDishesMatchingCriterias(
+                    It.IsAny<decimal>(),
+                    It.IsAny<int>(),
+                    It.Is<string>(name => name == searchBy),
+                    It.IsAny<IList<long>>())).Returns(Task.FromResult(expectedDishes));
+
+            var controller = new DishController(serviceStub.Object);
+            //Act
+            var filterDto = new FilterDtoSearchObjectDto { MaxPrice = expectedPrice, SearchBy = searchBy, MinLikes = minLikes, Categories = categorySearchDtoIds };
+            var expectedResult = new ResultObjectDto<DishDtoResult> { };
+            expectedResult.content = expectedDishes.Select(DishConverter.EntityToApi).ToList();
+            expectedResult.Pagination.Total = expectedDishes.Count();
+
+            var result = (ObjectResult)await controller.DishSearch(filterDto);
+
+            //Assert
+            var compareObj = JsonConvert.SerializeObject(expectedResult);
+            Assert.Equal(compareObj, result.Value);
+        }
+
+
+        [Fact]
+        public async Task DishSearch_WithFullFilterCriteria_ReturnsAllCorrespondingDishes()
+        {
+            //Arrange
+            var dishes = new[] { CreateRandomDish(), CreateRandomDish(), CreateRandomDish() };
+            var dishPrices = new[] { dishes[0].Price, dishes[1].Price, dishes[2].Price };
+
+            //Price Filter is set to max. Therefore all dishes will be taken into account.
+            var expectedPrice = dishPrices.Max();
+            var minLikes = rand.Next();
+
+            //Choose randomly one of the Dish names to filter for
+            var dishNames = new[] { dishes[0].Name, dishes[1].Name, dishes[2].Name };
+            var searchBy = dishNames[rand.Next(0, 3)];
+
+            List<long> categoryIds = new List<long>() { dishes[0].DishCategory.ToList().FirstOrDefault().Id, dishes[1].DishCategory.ToList().FirstOrDefault().Id };
+            
+            var categorySearchDtoIds = categoryIds.Select(c => new CategorySearchDto
+            {
+                Id = c,
+            }).ToList().ToArray();
+
+            List<Dish> expectedDishes = dishes.Where(dish =>
+                dish.DishCategory.Any(cat => categoryIds.Contains(cat.Id)) &&
+                dish.Price <= expectedPrice &&
+                dish.Name.Contains(searchBy, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+            var serviceStub = new Mock<IDishService>();
+            serviceStub.Setup(repo => repo.GetDishesMatchingCriterias(
+                    //It.IsAny<decimal>(),
+                    It.Is<decimal>(price => price == expectedPrice),
+                    It.Is<int>(likes => likes == minLikes),
+                    It.Is<string>(searchCriteria => searchCriteria == searchBy),
+                    It.Is<IList<long>>(categoryIdList => categoryIdList.All(cat => categoryIds.Contains(cat)))
+                    )).Returns(Task.FromResult(expectedDishes));
+
+            var controller = new DishController(serviceStub.Object);
+            //Act
+
+            var filterDto = new FilterDtoSearchObjectDto { MaxPrice = expectedPrice, SearchBy = searchBy, MinLikes = minLikes, Categories = categorySearchDtoIds };
+            var expectedResult = new ResultObjectDto<DishDtoResult> { };
+            expectedResult.content = expectedDishes.Select(DishConverter.EntityToApi).ToList();
+            expectedResult.Pagination.Total = expectedDishes.Count();
+
+            var result = (ObjectResult)await controller.DishSearch(filterDto);
+
+            //Assert
+            var compareObj = JsonConvert.SerializeObject(expectedResult);
+            Assert.Equal(compareObj, result.Value);
+        }
+
+        
+        [Fact]
+        public async Task DishSearch_WithNullFilterCriteria_ReturnsDefault()
+        {
+            //Arrange
+            List<Dish> Dishes = new List<Dish> { CreateRandomDish(), CreateRandomDish(), CreateRandomDish() };
+            var serviceStub = new Mock<IDishService>();
+
+            serviceStub.Setup(repo => repo.GetDishesMatchingCriterias(
+                    It.IsAny<decimal>(),
+                    It.IsAny<int>(),
+                    It.IsAny<string>(),
+                    It.IsAny<IList<long>>())).Returns(Task.FromResult(Dishes));
+
+            var controller = new DishController(serviceStub.Object);
+            //Act
+            var expectedResult = new ResultObjectDto<DishDtoResult> { };
+            expectedResult.content = Dishes.Select(DishConverter.EntityToApi).ToList();
+            expectedResult.Pagination.Total = Dishes.Count();
+
+            var result = (ObjectResult)await controller.DishSearch(null);
+
+            //Assert
+            var compareObj = JsonConvert.SerializeObject(expectedResult);
+            Assert.Equal(compareObj, result.Value);
+        }
+
+        private DishCategory CreateRandomCategory()
+        {
+            return new()
+            {
+                Id = rand.NextInt64(),
+            };
+        }
+
+        private Dish CreateRandomDish()
+        {
+            return new()
+            {
+                Id = rand.NextInt64(),
+                Name = Guid.NewGuid().ToString(),
+                Price = ((decimal)rand.NextDouble() + rand.Next()),
+                DishCategory = new[] { CreateRandomCategory(), CreateRandomCategory(), CreateRandomCategory() }
+            };
+        }
     }
 }
--- Templates/WebAPI/Devon4Net.Test/Test/UnitTest/Business/DishManagement/Service/DishServiceTest.cs
+++ Templates/WebAPI/Devon4Net.Test/Test/UnitTest/Business/DishManagement/Service/DishServiceTest.cs
@@ -10,31 +10,51 @@ using Devon4Net.Application.WebAPI.Implementation.Domain.RepositoryInterfaces;
 using System.Linq.Expressions;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using System.Linq;
 namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
 {
     public class DishServiceTest : UnitTest
     {
-        [Fact]
-        public async void getDishesMatchingCriterias_Returns_Price_Less_Than_6()
+        public static IList<Dish> buildListOfExampleDishes()
         {
-            //Arrange
             IList<Dish> dishList = new List<Dish>();
 
+            DishCategory category1 = new DishCategory();
+            category1.IdCategory = 1;
+            DishCategory category2 = new DishCategory();
+            category2.IdCategory = 2;
+
             Dish dish1 = new Dish();
+            dish1.Id = 1;
             dish1.Name = "falafel";
             dish1.Price = 6;
+            dish1.DishCategory.Add(category1);
 
             Dish dish2 = new Dish();
+            dish2.Id = 2;
             dish2.Name = "schnitzel";
             dish2.Price = 8;
+            dish2.DishCategory.Add(category2);
 
             Dish dish3 = new Dish();
+            dish3.Id = 3;
             dish3.Name = "salad";
             dish3.Price = 5;
+            dish3.DishCategory.Add(category1);
+            dish3.DishCategory.Add(category2);
 
             dishList.Add(dish1);
             dishList.Add(dish2);
             dishList.Add(dish3);
+
+            return dishList;
+        }
+
+        [Fact]
+        public async void getDishesMatchingCriterias_Returns_Price_Less_Than_6()
+        {
+            //Arrange
+            IList<Dish> dishList = buildListOfExampleDishes();
 
             var uowMock = new Mock<IUnitOfWork<ModelContext>>();
             var dishRepositoryMock = new Mock<IDishRepository>();
@@ -54,7 +74,7 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
             IList<long> categoryIdList = new List<long>();
 
             var expectedList = new List<Dish>();
-            expectedList.Add(dish3);
+            expectedList.Add(dishList[2]);
             var expectedResult = await Task.FromResult(expectedList);
             //Act
             var result = await dishService.GetDishesMatchingCriterias(maxPrice, minLikes, searchBy, categoryIdList);
@@ -66,32 +86,8 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
         [Fact]
         public async void getDishesMatchingCriterias_Where_CategoryId_Is_1()
         {
-            //Arrange
-            IList<Dish> dishList = new List<Dish>();
-            DishCategory category1 = new DishCategory();
-            category1.IdCategory = 1;
-            DishCategory category2 = new DishCategory();
-            category2.IdCategory = 2;
-
-            Dish dish1 = new Dish();
-            dish1.Name = "falafel";
-            dish1.Price = 6;
-            dish1.DishCategory.Add(category1);
-
-            Dish dish2 = new Dish();
-            dish2.Name = "schnitzel";
-            dish2.Price = 8;
-            dish2.DishCategory.Add(category2);
-
-            Dish dish3 = new Dish();
-            dish3.Name = "salad";
-            dish3.Price = 5;
-            dish3.DishCategory.Add(category1);
-            dish3.DishCategory.Add(category2);
-
-            dishList.Add(dish1);
-            dishList.Add(dish2);
-            dishList.Add(dish3);
+                        //Arrange
+            IList<Dish> dishList = buildListOfExampleDishes();
 
             var uowMock = new Mock<IUnitOfWork<ModelContext>>();
             var dishRepositoryMock = new Mock<IDishRepository>();
@@ -102,18 +98,20 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
                 )).Returns(
                     Task.FromResult(dishList)
                 );
+
             uowMock.Setup(uow => uow.Repository<IDishRepository>()).Returns(dishRepositoryMock.Object);
 
             DishService dishService = new DishService(uowMock.Object);
             decimal maxPrice = 0;
             int minLikes = 0;
-            string searchBy = "";
+            string searchBy = null;
             IList<long> categoryIdList = new List<long>();
             categoryIdList.Add(1);
 
             var expectedList = new List<Dish>();
-            expectedList.Add(dish1);
-            expectedList.Add(dish3);
+            expectedList.Add(dishList[0]);
+            expectedList.Add(dishList[2]);
+
             var expectedResult = await Task.FromResult(expectedList);
             //Act
             var result = await dishService.GetDishesMatchingCriterias(maxPrice, minLikes, searchBy, categoryIdList);
@@ -126,31 +124,7 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
         public async void getDishesMatchingCriterias_SearchBy()
         {
             //Arrange
-            IList<Dish> dishList = new List<Dish>();
-            DishCategory category1 = new DishCategory();
-            category1.IdCategory = 1;
-            DishCategory category2 = new DishCategory();
-            category2.IdCategory = 2;
-
-            Dish dish1 = new Dish();
-            dish1.Name = "falafel";
-            dish1.Price = 6;
-            dish1.DishCategory.Add(category1);
-
-            Dish dish2 = new Dish();
-            dish2.Name = "schnitzel";
-            dish2.Price = 8;
-            dish2.DishCategory.Add(category2);
-
-            Dish dish3 = new Dish();
-            dish3.Name = "salad";
-            dish3.Price = 5;
-            dish3.DishCategory.Add(category1);
-            dish3.DishCategory.Add(category2);
-
-            dishList.Add(dish1);
-            dishList.Add(dish2);
-            dishList.Add(dish3);
+            IList<Dish> dishList = buildListOfExampleDishes();
 
             var uowMock = new Mock<IUnitOfWork<ModelContext>>();
             var dishRepositoryMock = new Mock<IDishRepository>();
@@ -169,9 +143,8 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
             string searchBy = "salad";
             IList<long> categoryIdList = new List<long>();
 
-
             var expectedList = new List<Dish>();
-            expectedList.Add(dish3);
+            expectedList.Add(dishList[2]);
 
             var expectedResult = await Task.FromResult(expectedList);
             //Act
